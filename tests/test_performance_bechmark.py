6d72f34c2665023854e1313c72ec09a5b9ca5820
shashanksingh <shashank.personal@gmail.com>
2020-08-03 04:05:52 +0100

    e2e test work

--- tests/test_performance_bechmark.py
+++ tests/test_performance_bechmark.py
@@ -11,12 +11,28 @@ def random_character():
     return random.choice(string.ascii_lowercase)
 
 
-def save_all_into_storage():
-    with MySqlContainer("mysql:8.0.21") as mysql:
-        engine = sqlalchemy.create_engine(mysql.get_connection_url())
-        orm = Orm(engine=engine)
-        orm.create_short_url(long_url=f"HTTP://MOCK_URL/{random_character()}")
+def save_all_into_storage(engine):
+    orm = Orm(engine=engine)
+    orm.create_short_url(long_url=f"HTTP://MOCK_URL/{random_character()}")
 
 
 # def test_performance(benchmark):
-#     benchmark(save_all_into_storage)
+#     with MySqlContainer("mysql:5.7.28") as mysql:
+#         engine = sqlalchemy.create_engine(mysql.get_connection_url())
+#         try:
+#             engine.execute(
+#                 """
+#                 CREATE TABLE IF NOT EXISTS Urls (
+#                     `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+#                     `long_url` varchar(2048) NOT NULL,
+#                     `short_url` varchar(255) NOT NULL,
+#                     `created_at` Timestamp DEFAULT CURRENT_TIMESTAMP,
+#                     `long_url_hash` varchar(512) NOT NULL, /* To enforce one long url is one short url*/
+#                     PRIMARY KEY (`id`),
+#                     UNIQUE INDEX long_url_hash_uniqueness_constraint (long_url_hash)
+#                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+#             """
+#             )
+#         except sqlalchemy.exc.InvalidRequestError:
+#             pass
+#         benchmark(save_all_into_storage, engine)
--- tests/test_url_shortner_service_controller.py
+++ tests/test_url_shortner_service_controller.py
@@ -6,75 +6,38 @@ from src.url_shortner_service_controller import UrlShortnerServiceController
 from testcontainers.mysql import MySqlContainer
 
 
-# def test_create_short_url(mocker):
-#     with MySqlContainer("mysql:5.7.28") as mysql:
-#         engine = sqlalchemy.create_engine(mysql.get_connection_url())
-#         engine.execute(
-#             """
-#             CREATE TABLE Urls (
-#                 `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-#                 `long_url` varchar(2048) NOT NULL,
-#                 `short_url` varchar(255) NOT NULL,
-#                 `created_at` Timestamp DEFAULT CURRENT_TIMESTAMP,
-#                 `long_url_hash` varchar(512) NOT NULL, /* To enforce one long url is one short url*/
-#                 PRIMARY KEY (`id`),
-#                 UNIQUE INDEX long_url_hash_uniqueness_constraint (long_url_hash) USING HASH
-#             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-#         """
-#         )
-#         orm = Orm(engine=engine)
-#         shortner = UrlShortnerServiceController()
-#         shortner.create_short_url(request=LongUrl(url="https://MOCKURL"), context=None)
-#         assert orm.get_all_short_urls()[0][1] == "https://MOCKURL"
-
-
-# def test_get_short_url_details(mocker):
-#     with MySqlContainer("mysql:5.7.28") as mysql:
-#         engine = sqlalchemy.create_engine(mysql.get_connection_url())
-#         engine.execute("""
-#             CREATE TABLE Urls (
-#                 `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-#                 `long_url` varchar(2048) NOT NULL,
-#                 `short_url` varchar(255) NOT NULL,
-#                 `created_at` Timestamp DEFAULT CURRENT_TIMESTAMP,
-#                 `long_url_hash` varchar(512) NOT NULL, /* To enforce one long url is one short url*/
-#                 PRIMARY KEY (`id`),
-#                 UNIQUE INDEX long_url_hash_uniqueness_constraint (long_url_hash) USING HASH
-#             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-#         """)
-#         orm = Orm(engine=engine)
-#         shortner = UrlShortnerServiceController()
-#         shortner.get(request=LongUrl(url="https://MOCKURL"), context=None)
-#         assert orm.get_all_short_urls()[0][1] == "https://MOCKURL"
-
-
-def test_get_all_short_urls():
+def test_e2e_from_controller_to_database_for_all_functionality():
     with MySqlContainer("mysql:5.7.28") as mysql:
         engine = sqlalchemy.create_engine(mysql.get_connection_url())
-        engine.execute(
+        try:
+            engine.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Urls (
+                    `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+                    `long_url` varchar(2048) NOT NULL,
+                    `short_url` varchar(255) NOT NULL,
+                    `created_at` Timestamp DEFAULT CURRENT_TIMESTAMP,
+                    `long_url_hash` varchar(512) NOT NULL, /* To enforce one long url is one short url*/
+                    PRIMARY KEY (`id`),
+                    UNIQUE INDEX long_url_hash_uniqueness_constraint (long_url_hash)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
             """
-            CREATE TABLE Urls (
-                `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-                `long_url` varchar(2048) NOT NULL,
-                `short_url` varchar(255) NOT NULL,
-                `created_at` Timestamp DEFAULT CURRENT_TIMESTAMP,
-                `long_url_hash` varchar(512) NOT NULL, /* To enforce one long url is one short url*/
-                PRIMARY KEY (`id`),
-                UNIQUE INDEX long_url_hash_uniqueness_constraint (long_url_hash)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        """
-        )
+            )
+        except sqlalchemy.exc.InvalidRequestError:
+            pass
         orm = Orm(engine=engine)
         shortner = UrlShortnerServiceController()
         shortner.create_short_url(
             request=LongUrl(url="https://MOCKURLONE"), context=None
         )
-        shortner.create_short_url(
+        short_url = shortner.create_short_url(
             request=LongUrl(url="https://MOCKURLTWO"), context=None
         )
 
-        assert [x[1] for x in orm.get_all_short_urls()].sort() == [
+        all_urls = orm.get_all_short_urls()
+        assert orm.get_short_url_details(short_url=all_urls[0][0])[0][1] == "https://MOCKURLONE"
+        assert [x[1] for x in all_urls].sort() == [
             "https://MOCKURLONE",
             "https://MOCKURLTWO",
         ].sort()
-        assert len(orm.get_all_short_urls()) == 2
+        assert len(all_urls) == 2
