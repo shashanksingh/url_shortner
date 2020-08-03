732b12653f9b96b156545e079bf8fb15c72b8032
shashanksingh <shashank.personal@gmail.com>
2020-08-03 02:48:24 +0100

    adding mor tests

--- tests/test_performance_bechmark.py
+++ tests/test_performance_bechmark.py
@@ -1,10 +1,15 @@
-# from src.url_shortner_service_controller import UrlShortnerServiceController
-#
-#
-# def save_all_into_storage():
-#     # shortner = UrlShortnerServiceController()
-#     # shortner.create_short_url(long_url="https://MOCK_URL")
-#     pass
-#
-# def test_performance_of_saving_all_graphs_into_storage(benchmark):
-#     benchmark(save_all_into_storage)
+import sqlalchemy
+
+from src.database.orm import Orm
+from testcontainers.mysql import MySqlContainer
+
+
+def save_all_into_storage():
+    with MySqlContainer("mysql:8.0.21") as mysql:
+        engine = sqlalchemy.create_engine(mysql.get_connection_url())
+        orm = Orm(engine=engine)
+        orm.create_short_url(long_url="HTTP://MOCK_URL")
+
+
+def test_performance_of_saving_all_graphs_into_storage(benchmark):
+    benchmark(save_all_into_storage)
