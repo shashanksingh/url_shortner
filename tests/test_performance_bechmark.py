5f5d082039305f38fc13322f86e64f9cbecf602a
shashanksingh <shashank.personal@gmail.com>
2020-08-03 02:52:18 +0100

    added a performance test

--- tests/test_performance_bechmark.py
+++ tests/test_performance_bechmark.py
@@ -1,14 +1,21 @@
+import random
+import string
+
 import sqlalchemy
 
 from src.database.orm import Orm
 from testcontainers.mysql import MySqlContainer
 
 
+def random_character():
+    return random.choice(string.ascii_lowercase)
+
+
 def save_all_into_storage():
     with MySqlContainer("mysql:8.0.21") as mysql:
         engine = sqlalchemy.create_engine(mysql.get_connection_url())
         orm = Orm(engine=engine)
-        orm.create_short_url(long_url="HTTP://MOCK_URL")
+        orm.create_short_url(long_url=f"HTTP://MOCK_URL/{random_character()}")
 
 
 def test_performance_of_saving_all_graphs_into_storage(benchmark):
